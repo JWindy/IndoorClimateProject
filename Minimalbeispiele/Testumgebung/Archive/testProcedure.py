9831e33b66f5437542613684df158c26be1b9ac3
Johannes Windmiller <j.windmiller@gmx.de>
2020-12-16 00:50:07 +0100

    update ipytest und ToC

---
+++ Minimalbeispiele/Testumgebung/Archive/testProcedure.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[14]:
+
+
+def testMySum():
+    assert mySumForToArguments(3, 4) == 7
+    assert mySumForIterable([3,4]) == 7
+    assert mySumForIterable([3,4]) == 8
+    
+    print("Test done")
+
+
+# In[ ]:
+
+
+
+
